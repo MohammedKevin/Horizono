a58cba7fe6e20f2bffb27a106dfc2dcbb9fe4fbe
Kevin Mohammed <kevinkamilmohammed16@gmail.com>
2020-03-02 11:22:59 +0100

    changed state machine

---
+++ Assets/Scripts/StateMachine/EndSceneScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndSceneScript : MonoBehaviour
+{
+    public StateController stateController;
+    public void EndSceneEvent()
+    {
+        stateController.NextState();
+    }
+}
