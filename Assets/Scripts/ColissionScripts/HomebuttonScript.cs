6a038ac6b53ee845f262d5d30b2bd84df7ae2054
lukasbochis <lukas.bochis@gmail.com>
2020-03-30 15:44:42 +0200

    You can start the animation of the phone by standing on the homebutton.

---
+++ Assets/Scripts/ColissionScripts/HomebuttonScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomebuttonScript : MonoBehaviour
+{
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponentInParent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (this.animator != null)
+        {
+            this.animator.SetTrigger("ColissionWithHomebuttonTrigger");
+            this.gameObject.SetActive(false);
+        }
+        else
+            Debug.Log("Collided with homebutton but animator not found!");
+    }
+}
