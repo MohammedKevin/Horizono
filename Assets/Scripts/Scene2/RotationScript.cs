b9d7e38e21e4ab3a477e13d948f3bce5c077e059
Kevin Mohammed <kevinkamilmohammed16@gmail.com>
2020-05-18 15:33:15 +0200

    rotate

--- Assets/Scripts/Scene2/RotationScript.cs
+++ Assets/Scripts/Scene2/RotationScript.cs
@@ -4,11 +4,10 @@ using UnityEngine;
 
 public class RotationScript : MonoBehaviour
 {
-    private int rotateStepSize = 360 / 26;
-    public char input = 'a';
-    private bool isRotating = false;
-    private float current = 0;
-    private float goal = 0;
+    private double rotateStepSize = 13.84615;
+    public char input = 'd';
+    private double current = 0;
+    private double goal = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,24 +17,25 @@ public class RotationScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        current = this.transform.eulerAngles.y;
+        Rotate(input);
+        Rotate(input);
+        Rotate(input);
+        Rotate(input);
         Rotate(input);
     }
     void Rotate(char letter)
     {
-        int rotation = (letter - 'a') * rotateStepSize;
-        goal = rotation;
-        if (current > goal -2 && current < goal + 2)
+        double goal = (letter - 'a') * rotateStepSize;
+        if (current > goal -0.1 && current < goal + 0.1)
         {
-            isRotating = false;
             return;
         }
         this.transform.eulerAngles = new Vector3
         {
             x = this.transform.eulerAngles.x,
-            y = this.transform.eulerAngles.y + 1,
-            z = this.transform.eulerAngles.z
+            y = this.transform.eulerAngles.y,
+            z = this.transform.eulerAngles.z+0.05f
         };
-        current = this.transform.eulerAngles.y;
+        current = this.transform.eulerAngles.z;
     }
 }
