dfeaa9bcf690a89dd3ee83b0b3ed5de226757c5b
Kevin Mohammed <kevinkamilmohammed16@gmail.com>
2020-05-18 17:30:12 +0200

    added animation rotx

--- Assets/Scripts/StateMachine/States/SceneTwoState.cs
+++ Assets/Scripts/StateMachine/States/SceneTwoState.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SceneTwoState : MonoBehaviour, IState
 {
@@ -28,5 +29,16 @@ public class SceneTwoState : MonoBehaviour, IState
     public void Input(string input)
     {
 
+    }
+
+    public void colorInLetter()
+    {
+        string currentLetter = this.gameObject.transform.Find("Text").Find("Letter").gameObject.GetComponent<Text>().text;
+        this.gameObject.transform.Find("BigLetterWheel").Find("Letters").Find(currentLetter).gameObject.GetComponent<Text>().color = Color.green;
+    }
+    public void colorOutLetter()
+    {
+        string currentLetter = this.gameObject.transform.Find("Text").Find("Letter").gameObject.GetComponent<Text>().text;
+        this.gameObject.transform.Find("BigLetterWheel").Find("Letters").Find(currentLetter).gameObject.GetComponent<Text>().color = Color.white;
     }
 }
