b3a2e6ee9b9d8e0757e1e2cf7003c1f1175baaeb
lukasbochis <lukas.bochis@gmail.com>
2020-06-12 11:43:08 +0200

    collission is detected when both collider have the same color

---
+++ Assets/Scripts/Scene3/SlotScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlotScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (this.name.Contains("Blue"))
+        {
+            if (other.name.Contains("Blue"))
+            {
+                Debug.Log("Packet Collided!");
+            }
+        }
+        else if (this.name.Contains("Green"))
+        {
+            if (other.name.Contains("Green"))
+            {
+                Debug.Log("Packet Collided!");
+            }
+        }
+        else if (this.name.Contains("Red"))
+        {
+            if (other.name.Contains("Red"))
+            {
+                Debug.Log("Packet Collided!");
+            }
+        }
+    }
+}
