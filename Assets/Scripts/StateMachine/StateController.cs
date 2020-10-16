6bcb2c917ae2f403c4b3d43edf0b297230771704
MohammedKevin <kevin.mohammed@pcode.at>
2020-10-16 07:40:42 +0200

    sat

--- Assets/Scripts/StateMachine/StateController.cs
+++ Assets/Scripts/StateMachine/StateController.cs
@@ -31,7 +31,7 @@ public class StateController : MonoBehaviour
     }
     public void getStates()
     {
-        string[] stateNames = {"/SceneOneState", "/SceneTwoState", "/SceneThreeState", "/SceneFourState", "/SceneFiveState" };
+        string[] stateNames = {"/SceneFourState", "/SceneFiveState" };
         foreach (var sceneName in stateNames)
         {
              scenes.AddLast(GameObject.Find(sceneName).GetComponent<IState>());
