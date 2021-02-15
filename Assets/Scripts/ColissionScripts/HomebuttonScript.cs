42db9b2b891694ae4eb5353acb560813bb78be8b
Lukas Bochis <32123791+lukasbochis@users.noreply.github.com>
2021-02-15 14:15:00 +0100

    Update HomebuttonScript.cs

--- Assets/Scripts/ColissionScripts/HomebuttonScript.cs
+++ Assets/Scripts/ColissionScripts/HomebuttonScript.cs
@@ -1,14 +1,28 @@
-﻿using System.Collections;
+﻿using DeepSpace;
+using DeepSpace.Udp;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class HomebuttonScript : MonoBehaviour
 {
     private Animator _animator;
+    private UdpCmdConfigMgr _udpCmdConfigMgr;
+    private UdpSender _udpSender;
+
     // Start is called before the first frame update
     void Start()
     {
         _animator = GetComponentInParent<Animator>();
+        _udpCmdConfigMgr = GameObject.Find("UdpCmdConfigMgr").GetComponent<UdpCmdConfigMgr>();
+
+        if (_udpCmdConfigMgr.applicationType == CmdConfigManager.AppType.FLOOR)
+            _udpSender = GameObject.Find("UdpSenderToWall").GetComponent<UdpSender>();
+        else
+            _udpSender = GameObject.Find("UdpSenderToFloor").GetComponent<UdpSender>();
+
+
+        Debug.Log(_udpCmdConfigMgr.applicationType.ToString());
     }
 
     // Update is called once per frame
@@ -21,6 +35,7 @@ public class HomebuttonScript : MonoBehaviour
     {
         if (this._animator != null)
         {
+            _udpSender.AddMessage("ColissionWithHomebuttonTrigger");
             this._animator.SetTrigger("ColissionWithHomebuttonTrigger");
         }
         else
