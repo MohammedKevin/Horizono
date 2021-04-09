6b7a56cd3711e57cf6cc4f27bbf2f73a1616597c
Lukas Bochis <32123791+lukasbochis@users.noreply.github.com>
2021-04-09 10:54:19 +0200

    implemented receiving message, when stepping on homebutton

---
+++ Assets/Scripts/NetworkScripts/MessageReceiver.cs
@@ -0,0 +1,40 @@
+﻿using DeepSpace.Udp;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using UnityEngine;
+
+public class MessageReceiver : MonoBehaviour
+{
+    public UdpReceiver udpReceiver;
+
+    private Animator _animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        udpReceiver.SubscribeReceiveEvent(ReceiveMessage);
+        _animator = GetComponentInParent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ReceiveMessage(byte[] messageBytes, IPAddress senderIP)
+    {
+        string message = Encoding.Default.GetString(messageBytes);
+
+        var splittedMessage = message.Split(';');
+
+        if (splittedMessage[0] == "Homebutton")
+        {
+            if (_animator != null)
+                _animator.SetTrigger(splittedMessage[1]);
+        }
+
+    }
+}
---
+++ Assets/Scripts/NetworkScripts/MessageSender.cs
@@ -0,0 +1,34 @@
+﻿using DeepSpace;
+using DeepSpace.Udp;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MessageSender : MonoBehaviour
+{
+    public UdpSender SenderToWall;
+    public UdpSender SenderToFloor;
+
+    private CmdConfigManager.AppType appType;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var configMgr = GameObject.Find("UdpCmdConfigMgr").GetComponent<UdpCmdConfigMgr>();
+        appType = configMgr.applicationType;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public new void SendMessage(string message)
+    {
+        if (appType == CmdConfigManager.AppType.FLOOR)
+            SenderToFloor.AddMessage(message);
+        else
+            SenderToWall.AddMessage(message);
+    }
+}
